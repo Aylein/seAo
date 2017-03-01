39d903c0e2d07fe470fd110bd1351fd2d539fcff
aylein <shikinami.asuka@foxmail.com>
2017-03-01 22:50:53 +0800

    koa

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const koa = require("koa");
 const app = new koa();
 
-const api = require("./api/api.js");
+const api = require("./api");
 
-// app.use(ctx => {
-//     ctx.body = "Hello World";
-// });
-console.log(api.routes());
 app.use(api.routes()).use(api.allowedMethods());
+app.use(ctx => {
+    ctx.body = "Hello World";
+});
+app.jsonSpaces = 0;
 
 app.listen(810);
